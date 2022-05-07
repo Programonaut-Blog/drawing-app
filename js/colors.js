d3d8300e5742cf4a4e70ae6a81bf4432852ebc66
Maximilian Kürschner <dev@maximilian-kuerschner.de>
2022-05-07 17:39:35 +0200

    add: drawing features

--- js/colors.js
+++ js/colors.js
@@ -4,5 +4,7 @@ const colors = {
     "red": "#ef4444",
     "green": "#22c55e",
     "blue": "#3b82f6",
-    "yellow": "#eab308"
+    "yellow": "#eab308",
+    "orange": "#f97316",
+    "violet": "#8b5cf6"
 }
--- js/main.js
+++ js/main.js
@@ -3,7 +3,14 @@ const canvas = document.getElementById("canvas");
 const width = 1920;
 const height = 1080;
 let mode = 'draw';
-let start = {};
+
+const activeEvents = {
+  "mousedown": undefined,
+  "mouseup": undefined,
+  "mousemove": undefined
+};
+
+let drawing = false;
 
 // context of the canvas
 const context = canvas.getContext("2d");
@@ -14,6 +21,7 @@ canvas.height = height;
 canvas.width = width;
 
 // select default size
+context.lineCap = 'round';
 document.getElementById('small').firstElementChild.click();
 document.getElementById('pen').firstElementChild.click();
 
@@ -22,17 +30,14 @@ window.addEventListener("load", () => {
 });
 
 function setSize(e, size) {
-  console.log(e)
+  context.lineWidth = size;
   selectSize(e);
 }
 
-function setMode(e, mode) {
 
-  selectMode(e, mode);
-}
-
-function setColor(e, params) {
-  
+function setColor(e, color) {
+  context.strokeStyle = colors[color];
+  context.fillStyle = colors[color];
   selectColor(e);
 }
 
@@ -45,30 +50,186 @@ function selectColor(e) {
   e.target.classList.add('selected');
 }
 
-function selectMode(e, mode) {
+function selectMode(e, newMode) {
   const tools = document.getElementsByClassName("tool");
   for (const tool of tools) {
     tool.classList.remove('selected');
   }
-
+  
   const size = document.querySelector(".size.selected");
   if (size !== undefined)
     size.classList.remove('hide-select');
-
-  if (mode === 'fill' || mode === 'rect')
-    size.classList.add('hide-select');
-
+    
+  if (newMode === 'rect')
+  size.classList.add('hide-select');
+  
   e.target.parentElement.classList.add('selected');
+
+  mode = newMode;
 }
 
 function selectSize(e) {
+  if (mode === 'rect')
+    return;
+
   const sizes = document.getElementsByClassName("size");
   for (const size of sizes) {
     size.classList.remove('selected');
   }
-
+  
   if (e === undefined)
+  return;
+  
+  e.target.parentElement.classList.add('selected');
+}
+
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect(), // abs. size of element
+    scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for x
+    scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for y
+
+  return {
+    x: (evt.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
+    y: (evt.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+  }
+}
+
+function setMode(e, mode) {
+  for (const event in activeEvents) {
+    window.removeEventListener(event, activeEvents[event]);
+    activeEvents[event] = undefined;
+  }
+
+  switch (mode) {
+    case 'pen':
+      window.addEventListener("mousedown", startDraw);
+      window.addEventListener("mouseup", endDraw);
+      window.addEventListener("mousemove", draw);
+
+      activeEvents['mousedown'] = startDraw;
+      activeEvents['mouseup'] = endDraw;
+      activeEvents['mousemove'] = draw;
+      break;
+    case 'path':
+      window.addEventListener("mousedown", startPath);
+      window.addEventListener("mouseup", endPath);
+
+      activeEvents['mousedown'] = startPath;
+      activeEvents['mouseup'] = endPath;
+      break;
+    case 'polygon':
+      window.addEventListener("mousedown", startPolygon);
+      window.addEventListener("mouseup", endPolygon);
+
+      activeEvents['mousedown'] = startPolygon;
+      activeEvents['mouseup'] = endPolygon;
+      break;
+    case 'rect':
+      window.addEventListener("mousedown", startRect);
+      window.addEventListener("mouseup", endRect);
+
+      activeEvents['mousedown'] = startRect;
+      activeEvents['mouseup'] = endRect;
+      break;
+
+    default:
+      break;
+  }
+
+
+  selectMode(e, mode);
+}
+
+function startDraw(e) {
+  drawing = true;
+  context.beginPath();
+  draw(e)
+}
+
+function endDraw(e) {
+  drawing = false;
+}
+
+function draw(e) {
+  if (!drawing) return;
+
+  let { x, y } = getMousePos(canvas, e);
+
+  context.lineTo(x, y);
+  context.stroke();
+
+  // for smoother drawing
+  context.beginPath();
+  context.moveTo(x, y);
+}
+
+function startPath(e) {
+  drawing = true;
+  context.beginPath();
+  draw(e)
+}
+
+function endPath(e) {
+  drawing = false;
+  let { x, y } = getMousePos(canvas, e);
+  
+  context.lineTo(x, y);
+  context.stroke();
+}
+
+let poly = false;
+let polyTimeout = undefined;
+
+function startPolygon(e) {
+  if (e.target.id !== 'canvas')
     return;
 
-  e.target.parentElement.classList.add('selected');
+  drawing = true;
+
+  if (poly) {
+    polygon(e);
+  }
+  else {
+    context.beginPath();
+    draw(e);
+  }
+  poly = true;
+}
+
+function endPolygon(e) {
+  if (!poly)
+    return;
+
+  polyTimeout = setTimeout(() => {
+    drawing = false;
+    context.closePath();
+    context.stroke();
+
+    poly = false;
+  }, 1000);
+}
+
+function polygon(e) {
+  if (!drawing) return;
+  clearTimeout(polyTimeout);
+
+  let { x, y } = getMousePos(canvas, e);
+  
+  context.lineTo(x, y);
+  context.stroke();
+}
+
+let start = {}
+
+function startRect(e) {
+    start = getMousePos(canvas, e);
+}
+
+function endRect(e) {
+    let { x, y } = getMousePos(canvas, e);
+    context.fillRect(start.x, start.y, x - start.x, y - start.y);
+}
+
+function clearCanvas() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
 }
